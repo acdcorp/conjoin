1ae7c320a62d17443c73cd8b72dd00dab997eef6
Bernardo Galindo <bernardo@discoveryspecialists.com>
2018-09-14 15:41:22 -0500

    updates validations that verifies rails is present and the auth gems warden and devise

--- lib/conjoin/env_string.rb
+++ lib/conjoin/env_string.rb
@@ -7,11 +7,16 @@ module Conjoin
     end
 
     def mounted?
-      defined?(::Rails) ? true : false
+      mounted_on_rails? ? true : false
     end
 
     def console?
       ENV['CONJOIN_CONSOLE'] ? true : false
     end
+
+    private
+      def mounted_on_rails?
+        !!(defined?(::Rails) && defined?(::Warden) && defined?(::Devise))
+      end
   end
 end
